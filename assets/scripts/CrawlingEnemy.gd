53ce1280b12872a0f6ff27b1556de3f4efdbd955
toppop7 <65695818+toppop7@users.noreply.github.com>
2023-07-06 15:54:05 -0500

    Bobbing heads

--- assets/scripts/CrawlingEnemy.gd
+++ assets/scripts/CrawlingEnemy.gd
@@ -1,7 +1,7 @@
-extends CharacterBody3D
+extends RigidBody3D
 
 var player = null
-const SPEED = 4.0
+var SPEED = 4
 var npoint = Vector3()
 
 @export var player_path : NodePath
@@ -11,14 +11,18 @@ var heels = []
 var moving = []
 var foot_step = 0
 
+
+
 func _ready():
 	player = get_node(player_path)
 	feet = [$Foot1,$Foot2,$Foot3,$Foot4]
 	heels = [$Head/Heel1,$Head/Heel2,$Head/Heel3,$Head/Heel4]
 	moving = [false,false,false,false]
-
-func set_done():
-	moving[0] = false
+	
+	set_freeze_enabled(true)
+	await get_tree().create_timer(randi() % 4).timeout
+	set_freeze_enabled(false)
+	
 
 
 func update_feet():
@@ -44,33 +48,32 @@ func update_feet():
 				h2.origin += Vector3(0,-0.8,0)
 				
 				if moving[i] == false:
+					#Step1 makes the feet walk
 					moving[i] = true
 					var step1 = get_tree().create_tween()
-					step1.tween_property(foot,"transform",h2,0.2/2)
+					step1.tween_property(foot,"transform",h2,0.2)
+					step1.tween_property(foot,"transform",h,0.2)
 					
-					var step2end = func():
+					var step1end = func():
 						moving[i] = false
 						step1.kill()
 						foot_step = (foot_step + 1) % 4	
-						
-					var step1end = func():
-						var step2 = get_tree().create_tween()
-						step2.tween_property(foot,"transform",h,0.2/2)
-						step2.tween_callback(step2end)
-						
-
 					step1.tween_callback(step1end)
+					
+					#Step2 makes the head bob
+					var step2 = get_tree().create_tween()
+					var h3 = $Head.global_transform.interpolate_with(foot.global_transform,0.2)
+					step2.tween_property($Head/HeadMesh,"global_position",h3.origin,0.2)
+					step2.tween_property($Head/HeadMesh,"global_position",$Head.global_transform.origin,0.2)
 		
 
 
-func _physics_process(delta):
+func _integrate_forces(state):
 	nav_agent.set_target_position(player.global_transform.origin)
 	npoint = nav_agent.get_next_path_position()
-	velocity = (npoint - global_transform.origin).normalized() * SPEED
-	$Head.look_at(player.global_transform.origin)
+	state.linear_velocity = (npoint - global_transform.origin).normalized() * SPEED
+	$Head.look_at(player.global_transform.origin + Vector3(0,1,0))
 	
-	move_and_slide()
-
 	
 	update_feet()
 	
